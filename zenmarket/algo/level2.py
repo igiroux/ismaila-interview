d3f73a5f99d6cba16ad5a9ed195e4b3182a4aebf
Ismaila Giroux <igiroux@olfeo.com>
2017-02-02 22:22:43 +0100

    [level2] 1st draft

---
+++ zenmarket/algo/level2.py
@@ -0,0 +1,93 @@
+'''
+Level 2 costing: Compute delivery fees
+'''
+import numbers
+from typing import List, Tuple, NewType
+import bisect
+
+# pylint: disable=C0103
+
+Price = NewType('Price', numbers.Number)
+PriceRange = Tuple[List[Price], List[Price]]
+
+
+class DeliveryFeeKeyError(KeyError):
+    '''
+    Exception raised when infortmation is missing
+    '''
+    pass
+
+
+class DeliveryFeeValueError(ValueError):
+    '''
+    Exception raised when infortmation is missing
+    '''
+    pass
+
+
+def delivery_fees(price: Price, cost_function: PriceRange):
+    '''
+    :returns
+    '''
+    x, fx = cost_function
+    return price + fx[bisect.bisect_right(x, price)]
+
+
+def fee_data_to_cost_table(fee_data: List[dict]) -> PriceRange:
+    '''
+    [
+        {
+          "eligible_transaction_volume": {
+            "min_price": 0,
+            "max_price": 1000
+          },
+          "price": 800
+        },
+        {
+          "eligible_transaction_volume": {
+            "min_price": 1000,
+            "max_price": 2000
+          },
+          "price": 400
+        },
+        {
+          "eligible_transaction_volume": {
+            "min_price": 2000,
+            "max_price": None
+          },
+          "price": 0
+        },
+    ]
+
+    :returns [
+        [1000, 2000, float('+Inf')],
+        [800, 400, 0],
+    ]
+    '''
+    def iter_price_info():
+        '''
+        Generator that yields price info
+        '''
+        for info in fee_data:
+            try:
+                min_price = info['eligible_transaction_volume']['min_price']
+                max_price = info['eligible_transaction_volume']['max_price']
+                cost = info['price']
+                min_price, max_price, cost = [
+                    float('+Inf') if x is None else int(x)
+                    for x in (min_price, max_price, cost)
+                ]
+            except KeyError:
+                raise DeliveryFeeKeyError(
+                    'Incomplete fee info: {}'.format(info))
+            except ValueError:
+                raise DeliveryFeeValueError(
+                    'Price value error: {}'.format(info))
+            else:
+                yield min_price, max_price, cost
+    prices = [
+        [max_price, float('+Inf')][int(max_price is not None)]
+        for _, max_price, _ in iter_price_info()
+    ]
+    fees = [cost for _, _, cost in iter_price_info()]
+    return prices, fees
---
+++ zenmarket/test/test_level2.py
@@ -0,0 +1,92 @@
+'''
+Level 2 pricing unit tests
+'''
+from random import randrange
+
+import pytest
+
+from zenmarket.algo.level2 import delivery_fees, fee_data_to_cost_table
+
+
+@pytest.fixture(name='fee_function')
+def fee_function_fixture():
+    '''
+    Fee Function Fixture: F3
+
+    F3: x -> y
+        0 -> 800
+     1000 -> 400
+     2000 -> 0
+    '''
+    return (
+        [1000, 2000, float('+Inf')],
+        [800, 400, 0]
+    )
+
+
+@pytest.fixture(scope='module', name='random_prices', params=(
+    ([randrange(0, 1000) for _ in range(30)], 800),
+    ([randrange(1000, 2000) for _ in range(30)], 400),
+    ([randrange(2000, 10000) for _ in range(30)], 0),
+))
+def random_prices_fixture(request):
+    '''
+    Random price fixture
+    '''
+    prices, fee = request.param
+    return ((price, price + fee) for price in prices)
+
+
+@pytest.fixture(name='fee_data')
+def fee_data_fixture():
+    '''
+    Fee input data fixture
+    '''
+    return [
+        {
+            "eligible_transaction_volume": {
+                "min_price": 0,
+                "max_price": 1000
+            },
+            "price": 800
+        },
+        {
+            "eligible_transaction_volume": {
+                "min_price": 1000,
+                "max_price": 2000
+            },
+            "price": 400
+        },
+        {
+            "eligible_transaction_volume": {
+                "min_price": 2000,
+                "max_price": None
+            },
+            "price": 0
+        },
+    ]
+
+
+def test_fee_interpolation(random_prices, fee_function):
+    '''
+    Interpolation should work between bounds
+    '''
+    sample = random_prices
+    for price, expected_cost in sample:
+        assert delivery_fees(price, fee_function) == expected_cost
+
+
+def test_fee_function_bounds(fee_function):
+    '''
+    Interpolation should work for bounds
+    '''
+    prices = [0, 1000, 2000]
+    fees = [800, 400, 0]
+    for price, fee in zip(prices, fees):
+        assert delivery_fees(price, fee_function) == price + fee
+
+
+def test_fee_data_as_cost_function(fee_data):
+    x, fx = fee_data_to_cost_table(fee_data)
+    assert tuple(x) == (1000, 2000, float('+Inf'))
+    assert tuple(fx) == (800, 400, 0)
