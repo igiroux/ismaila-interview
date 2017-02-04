94ceeef11c5e45c96f9f0eb7d9f21814ca02e657
Ismaila Giroux <igiroux@olfeo.com>
2017-02-04 22:49:26 +0100

    [level3] Level 3 pricing should apply discounts

--- zenmarket/algo/level3.py
+++ zenmarket/algo/level3.py
@@ -1,6 +1,10 @@
 '''
 This is simple cart pricing module
 '''
+from operator import itemgetter
+from typing import Callable, NewType
+
+from zenmarket.algo import level2
 
 
 def cart_price(cart: dict, articles: dict, discounts: dict) -> dict:
@@ -12,7 +16,7 @@ def cart_price(cart: dict, articles: dict, discounts: dict) -> dict:
     >>> cart_price()
     '''
 
-    def iter_cart_items_prices(items, articles):
+    def iter_cart_items_prices(items, articles, discounts):
         '''
         Generator that yields for each cart item (<cart_id>, <cart_item_price>)
         '''
@@ -20,11 +24,13 @@ def cart_price(cart: dict, articles: dict, discounts: dict) -> dict:
             try:
                 item_price = articles[item['article_id']]['price']
                 item_quantity = item['quantity']
+                item_discount_func = discounts.get(
+                    item['article_id'], lambda aprice: aprice)
             except KeyError:
                 raise ValueError(
                     'Cart item price could not be found {}'.format(item))
             else:
-                yield item_quantity, item_price
+                yield item_quantity, item_price, item_discount_func
 
     try:
         items = cart['items']
@@ -36,16 +42,162 @@ def cart_price(cart: dict, articles: dict, discounts: dict) -> dict:
         if not items:
             return {'id': cart_id, 'total': 0}
 
-        return {
+        resp = {
             'id': cart_id,
             'total': sum(
-                quantity * price
-                for quantity, price in iter_cart_items_prices(items, articles))
+                quantity * discount_fun(price)
+                for quantity, price, discount_fun in iter_cart_items_prices(
+                    items, articles, discounts
+                )
+            )
         }
+        return resp
 
 
-def discount(article_price, discount_type, discount_value):
-    if discount_type == "amount":
-        return article_price - discount_value
-    elif discount_type == "percentage":
-        return (100 - discount_value) * article_price / 100
+# pylint: disable=C0103
+DiscountFun = NewType('DiscountFun', Callable[[int], int])
+
+
+def discount_as_func(discount_type: str, discount_value: int) -> DiscountFun:
+    '''
+    :returns a discount function which applies a discount to a price
+    '''
+    switch = {
+        "amount": lambda aprice: aprice - discount_value,
+        "percentage": lambda aprice: aprice * (100 - discount_value) // 100
+    }
+    return switch.get(discount_type, lambda aprice: aprice)
+
+
+def price(data: dict) -> dict:
+    '''
+    Compute cart object price
+
+    :param data dict: {'articles': [...], 'carts': [...]}
+    :returns {'carts': [{'id': <id>, 'total': <total>}, ...]}
+    E.g.:
+
+    >>> data = {
+    ...     "articles": [
+    ...       {"id": 1, "name": "water", "price": 100},
+    ...       {"id": 4, "name": "tea", "price": 1000},
+    ...     ],
+    ...     "carts": [
+    ...       {
+    ...         "id": 1,
+    ...         "items": [
+    ...           { "article_id": 1, "quantity": 6 },
+    ...           { "article_id": 4, "quantity": 1 }
+    ...         ]
+    ...       },
+    ...       {
+    ...         "id": 3,
+    ...         "items": []
+    ...       }
+    ...     ],
+    ...     "delivery_fees": [
+    ...         {
+    ...             "eligible_transaction_volume": {
+    ...                 "min_price": 0,
+    ...                 "max_price": 1000
+    ...             },
+    ...             "price": 800
+    ...         },
+    ...         {
+    ...             "eligible_transaction_volume": {
+    ...                 "min_price": 1000,
+    ...                 "max_price": 2000
+    ...             },
+    ...             "price": 400
+    ...         },
+    ...         {
+    ...             "eligible_transaction_volume": {
+    ...                 "min_price": 2000,
+    ...                 "max_price": None
+    ...             },
+    ...             "price": 0
+    ...         },
+    ...     ],
+    ...     "discounts": [
+    ...         {"article_id": 2, "type": "amount", "value": 25},
+    ...         {"article_id": 5, "type": "percentage", "value": 30},
+    ...     ],
+    ... }
+
+    >>> print(price({}))
+    {'carts': []}
+
+    >>> print(price({"articles": [], carts: []}))
+    {'carts': []}
+
+    >>> print(price({
+    ...     "articles": [
+    ...         { "id": 10, "name": "tea", "price": 100 }
+    ...         { "id": 20, "name": "tea", "price": 200 }
+    ...         { "id": 40, "name": "tea", "price": 400 }
+    ...     ],
+    ...     "carts": [
+    ...         {"id": 1, items: [
+    ...             { "article_id": 10, "quantity": 2 },
+    ...             { "article_id": 20, "quantity": 1 },
+    ...             { "article_id": 30, "quantity": 1 },
+    ...         ]}
+    ...     ],
+    ...     "delivery_fees": [
+    ...         {
+    ...             "eligible_transaction_volume": {
+    ...                 "min_price": 0,
+    ...                 "max_price": 1000
+    ...             },
+    ...             "price": 800
+    ...         },
+    ...         {
+    ...             "eligible_transaction_volume": {
+    ...                 "min_price": 1000,
+    ...                 "max_price": 2000
+    ...             },
+    ...             "price": 400
+    ...         },
+    ...         {
+    ...             "eligible_transaction_volume": {
+    ...                 "min_price": 2000,
+    ...                 "max_price": None
+    ...             },
+    ...             "price": 0
+    ...         },
+    ...     ],
+    ...     "discounts": [
+    ...         {"article_id": 20, "type": "amount", "value": 20},
+    ...         {"article_id": 30, "type": "percentage", "value": 10},
+    ...     ],
+
+    ... }))
+    {'carts': [{'id': 1, 'total': 1540}, ]}
+
+    '''
+
+    if not data:
+        return {'carts': []}
+
+    params = itemgetter('type', 'value')  # discount params
+    article_id = itemgetter('article_id')
+
+    try:
+        articles = {article['id']: article for article in data["articles"]}
+        carts = iter(data["carts"])
+        discounts = {
+            article_id(discount): discount_as_func(*params(discount))
+            for discount in iter(data['discounts'])
+        }
+        fees_data = data['delivery_fees']
+    except KeyError:
+        raise ValueError('Invalid input data {}. \n'.format(data))
+    except TypeError:
+        raise ValueError(
+            'Invalid input value for key "carts". Expected Iterable')
+    else:
+        cost_func = level2.fee_data_to_cost_table(fees_data)
+        return {'carts': [
+            level2.plus_fees(cart_price(cart, articles, discounts), cost_func)
+            for cart in carts
+        ]}
--- zenmarket/test/test_level3.py
+++ zenmarket/test/test_level3.py
@@ -1,8 +1,121 @@
 '''
 Level 3 pricing tests
 '''
-from zenmarket.algo.level3 import discount
+import pytest
+from zenmarket.algo import level3
+
 
 def test_discount():
-    assert discount(100, 'amount', 10) == 90
-    assert discount(300, 'percentage', 10) == 270
+    '''
+    Test discounting
+    '''
+    assert level3.discount_as_func('amount', 10)(100) == 90
+    assert level3.discount_as_func('percentage', 10)(300) == 270
+
+
+@pytest.fixture(scope='module', name='simple_cart', params=[
+    ([{"article_id": 4, "quantity": 1}], 1 * 1000 + 400),
+    ([{"article_id": 4, "quantity": 2}], 2 * 1000 + 0),
+    ([
+        {"article_id": 4, "quantity": 1},  # 1 * 1000
+        {"article_id": 1, "quantity": 1},  # 1 * 100
+    ], 1100 + 400),
+    ([
+        {"article_id": 5, "quantity": 1},  # 1 * 999 * (100 - 30) // 100
+    ], 1 * 999 * (100 - 30) // 100 + 800),
+    ([
+        {"article_id": 5, "quantity": 1},  # _ 999 * (100 - 30) // 100
+        {"article_id": 6, "quantity": 1},  # + 999 < 2000
+    ], 999 * (100 - 30) // 100 + 999 + 400),
+    ([
+        {"article_id": 4, "quantity": 2},  # _ 2 * 1000
+    ], 2 * 1000 + 0),
+    ([
+        {"article_id": 2, "quantity": 10},  # + 10 * (200 - 25)
+    ], 10 * (200 - 25) + 400),
+    ([
+        {"article_id": 3, "quantity": 1},  # + 1 * 400
+    ], 1 * 400 + 800),
+    ([
+        {"article_id": 5, "quantity": 2},  # + 2 * 999 * (100 - 30) // 100
+    ], 2 * 999 * (100 - 30) // 100 + 400),
+    ([
+        {"article_id": 4, "quantity": 2},  # _ 2 * 1000
+        {"article_id": 2, "quantity": 10},  # 10 * (200 - 25)
+        {"article_id": 3, "quantity": 1},  # _ 1 * 400
+        {"article_id": 5, "quantity": 2},  # _ 2 * 999 * (100 - 30) // 100
+    ], 2 * 1000 + 10 * (200 - 25) + 1 * 400 + 2 * 999 * (100 - 30) // 100),
+])
+def simple_cart_input_data_fixture(request):
+    '''
+    Empty dict fixture
+    '''
+    cart_items, total_price = request.param
+    return total_price, {
+        "articles": [
+            {"id": 1, "name": "tea", "price": 100},
+            {"id": 2, "name": "honey", "price": 200},
+            {"id": 3, "name": "tea", "price": 400},
+            {"id": 4, "name": "tea", "price": 1000},
+            {"id": 5, "name": "ketchup", "price": 999},
+            {"id": 6, "name": "mayonnaise", "price": 999},
+
+        ],
+        "carts": [{'id': 10, 'items': cart_items}],
+        "delivery_fees": [
+            {
+                "eligible_transaction_volume": {
+                    "min_price": 0,
+                    "max_price": 1000
+                },
+                "price": 800
+            },
+            {
+                "eligible_transaction_volume": {
+                    "min_price": 1000,
+                    "max_price": 2000
+                },
+                "price": 400
+            },
+            {
+                "eligible_transaction_volume": {
+                    "min_price": 2000,
+                    "max_price": None
+                },
+                "price": 0
+            },
+        ],
+        "discounts": [
+            {"article_id": 2, "type": "amount", "value": 25},
+            {"article_id": 5, "type": "percentage", "value": 30},
+        ],
+    }
+
+
+def _test_response_format(response):
+    assert response and isinstance(response, dict)
+    assert "carts" in response
+
+
+def _test_plain_response_format(response):
+    _test_response_format(response)
+    assert response["carts"]
+    assert isinstance(response["carts"], (list, tuple))
+    for cart in response["carts"]:
+        assert isinstance(cart, dict)
+        assert cart
+        assert "id" in cart
+        assert "total" in cart
+
+
+def test_discounted_simple_cart(simple_cart):
+    '''
+    One cart price returns the price of the article
+    '''
+    total_price, data = simple_cart
+    resp = level3.price(data)
+    _test_plain_response_format(resp)
+    assert len(resp["carts"]) == 1
+    cart = resp["carts"][0]
+    assert cart["id"] == 10
+    assert cart["total"] == total_price, data['carts']
